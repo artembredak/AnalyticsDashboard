d4c2b330651e762602c90d86d8f54112131d91c0
Artem Bredak <artembredakk@gmail.com>
2026-01-08 00:54:43 +0100

    tests configuration

--- src/test/java/com/cloudcharts/cloudchartsproj/CloudChartsControllerTest.java
+++ src/test/java/com/cloudcharts/cloudchartsproj/CloudChartsControllerTest.java
@@ -1,9 +1,12 @@
 package com.cloudcharts.cloudchartsproj;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,6 +18,14 @@ class CloudChartsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+        Dotenv dotenv = Dotenv.load();
+        registry.add("spring.datasource.url", () -> dotenv.get("DB_URL"));
+        registry.add("spring.datasource.username", () -> dotenv.get("DB_USERNAME"));
+        registry.add("spring.datasource.password", () -> dotenv.get("DB_PASSWORD"));
+    }
 
     // ===================== TEST 1: APP VISITS =====================
     @Test
