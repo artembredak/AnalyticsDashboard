e9fd56f2b01099ad525a3cce519323a1a3d4617b
Artem Bredak <artembredakk@gmail.com>
2026-01-08 10:43:51 +0100

    google cloud requirements configuring

--- src/main/java/com/cloudcharts/cloudchartsproj/CloudChartsProjApplication.java
+++ src/main/java/com/cloudcharts/cloudchartsproj/CloudChartsProjApplication.java
@@ -1,6 +1,5 @@
 package com.cloudcharts.cloudchartsproj;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudChartsProjApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(CloudChartsProjApplication.class, args);
     }
 
